c19cb5e4db7119b625532d537f7c13773134a0a0
KANET KAMPIRANON <kanet.kampiranon@scb.co.th>
2021-06-15 16:47:25 +0700

    End of section 9

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -10,7 +10,7 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        public DateTime DateofBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime LastActive { get; set; } = DateTime.Now;
@@ -18,13 +18,13 @@ namespace API.Entities
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
-        public string city { get; set; }
+        public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
 
         // public int GetAge()
-        // // {
-        // //     return DateofBirth.CalculateAge();
+        // {
+        //     return DateOfBirth.CalculateAge();
         // }
 
     }
--- API/Extensions/DateTimeExtensions.cs
+++ API/Extensions/DateTimeExtensions.cs
@@ -2,12 +2,14 @@ using System;
 
 namespace API.Extensions
 {
-    public static class ExtenDateTime
+    public static class DateTimeExtensions
     {
         public static int CalculateAge(this DateTime dob)
         {
             var today = DateTime.Today;
             var age = today.Year - dob.Year;
+            // Console.WriteLine(DateTime.Today);
+            // Console.WriteLine(dob);
             if (dob.Date > today.AddYears(-age)) age--;
             return age;
         }
